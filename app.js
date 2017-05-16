d290a7dd878eef60d03ce3068434f669d1ad9295
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-05-16 08:39:47 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+// Packages
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Config
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, './static')));
+
+// Bodyparser for handling form
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var groceryItems = [];
+
+// Post data from form
+app.post('/', function(req, res) {
+  // Push data of grocery-items to empty array 'groceryItems'
+	groceryItems.push(req.body.groceryItem);
+	res.redirect('/');
+})
+
+// Get data from form
+app.get('/', function(req, res) {
+  // Pass data form form by template ejs
+	res.render('index', {groceryItems: groceryItems});
+});
+
+// Clear content .my-grocery-list
+app.get('/clear', function(req, res) {
+  // Put array 'groceryItems' on empty that make clear-option possible
+	groceryItems = [];
+	res.render('index', {groceryItems: groceryItems});
+});
+
+// Start server
+var server = app.listen(7000,function(){
+	console.log('Server Started on Port 7000');
+});
