52b2d312a0aaa67e2dd791b35599590b4b23f943
Nooroel Imamdi <nooroel.imamdi@hva.nl>
2017-07-09 20:13:13 +0200

    add feature detection and cursor image on all img

--- static/js/script.js
+++ static/js/script.js
@@ -1,39 +1,39 @@
 // html elements
 var groceryList = document.querySelectorAll('.my-grocery-list');
+var groceryListWrap = document.querySelector('.my-grocery-list-wrap');
 var img = document.querySelectorAll('img');
-var cursorMove = document.querySelector('img');
 var userChoices = document.querySelectorAll('input');
 var onlySelect = document.querySelector('.only-select');
 var dragandropAvailable = document.querySelector('.dragandrop-available');
 
 // feature detection if draggable and classList is available
 if('classList' in document.documentElement && 'draggable' in document.createElement('span')) {
-	onlySelect.classList.add('hide');
+
 	dragandropAvailable.classList.remove('hide');
-	cursorMove.classList.add('cursor');
-	console.log('classList and draggable supported by current browser');
-} else {
-	console.log('classList and draggable doesnt supported by current browser');
-}
 
-function drag(e) {
-	e.dataTransfer.setData('id', e.target.id);
-}
+	function drag(e) {
+		e.dataTransfer.setData('id', e.target.id);
+	}
 
-function dropToggle(e) {
-	e.preventDefault();
-}
+	function dropToggle(e) {
+		e.preventDefault();
 
-function drop(e) {
-	e.preventDefault();
-	var id = e.dataTransfer.getData('id');
-	userChoices[id].click();
-	this.classList.add('succes-drop');
-	setTimeout(1000);
-}
-if (Array.prototype.forEach) {
+		// delay to show feedback clear
+		groceryListWrap.classList.add('indication-drop');
+	}
+
+	function drop(e) {
+		e.preventDefault();
+		var id = e.dataTransfer.getData('id');
+		userChoices[id].click();
+		// delay to show feedback clear
+		groceryListWrap.classList.add('succes-drop');
+		setTimeout(4000);
+	}
+
 	img.forEach(function(img) {
 	  img.addEventListener('dragstart', drag);
+		img.classList.add('cursor');
 	});
 
 	groceryList.forEach(function(e) {
@@ -47,4 +47,7 @@ if (Array.prototype.forEach) {
 	groceryList.forEach(function(e) {
 	  e.addEventListener('drop', drop);
 	});
+	console.log('classList and draggable supported by current browser');
+} else {
+	console.log('classList and draggable doesnt supported by current browser');
 }
