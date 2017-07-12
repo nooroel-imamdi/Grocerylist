081afa1e90ae3df7ceda6348f10005ab243e2709
Nooroel Imamdi <nooroel.imamdi@hva.nl>
2017-07-13 00:42:36 +0200

    complete feature detection

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,18 @@
-// html elements
-var groceryList = document.querySelectorAll('.my-grocery-list');
-var groceryListWrap = document.querySelector('.my-grocery-list-wrap');
-var img = document.querySelectorAll('img');
-var userChoices = document.querySelectorAll('input');
-var onlySelect = document.querySelector('.only-select');
-var dragandropAvailable = document.querySelector('.dragandrop-available');
-
-// feature detection on classList, draggable, addEventListener, forEach
+// feature detection
 if('classList' in document.documentElement
 		&& 'draggable' in document.createElement('span')
 		&& 'addEventListener' in window
 		&& window.NodeList
 		&& NodeList.prototype.forEach
 		&& "files" in DataTransfer.prototype) {
+
+	// html elements
+	var groceryList = document.querySelectorAll('.my-grocery-list');
+	var groceryListWrap = document.querySelector('.my-grocery-list-wrap');
+	var img = document.querySelectorAll('img');
+	var userChoices = document.querySelectorAll('input');
+	var onlySelect = document.querySelector('.only-select');
+	var dragandropAvailable = document.querySelector('.dragandrop-available');
 
 	dragandropAvailable.classList.remove('hide');
 
@@ -22,14 +22,13 @@ if('classList' in document.documentElement
 
 	function dropToggle(e) {
 		e.preventDefault();
-		groceryListWrap.classList.add('indication-drop');
+		groceryListWrap.classList.add('succes-drop');
 	}
 
 	function drop(e) {
 		e.preventDefault();
 		var id = e.dataTransfer.getData('id');
 		userChoices[id].click();
-		groceryListWrap.classList.remove('indication-drop');
 		groceryListWrap.classList.add('succes-drop');
 	}
 
